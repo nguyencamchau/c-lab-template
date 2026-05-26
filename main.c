cd05ca1d399117b44e5d8cf190240009985cd57b
nguyencamchau <nguyencamchau455@gmail.com>
2026-05-26 08:36:46 +0700

    Tao khung chuong trinh main

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// Hàm nhập dãy số nguyên
+void nhapMang(int a[], int n) {
+    
+}
+
+// Hàm xuất dãy số nguyên
+void xuatMang(int a[], int n) {
+  
+}
+
+// Hàm sắp xếp dãy số tăng dần (Thuật toán Bubble Sort)
+void sapXepTangDan(int a[], int n) {
+   
+}
+
+// Hàm main điều khiển chính
+int main() {
+    int n;
+    int a[100]; // Khai báo mảng tối đa 100 phần tử
+
+    printf("Nhap so luong phan tu cua mang: ");
+    scanf("%d", &n);
+
+    // Gọi hàm nhập mảng
+    printf("--- Nhap mang ---\n");
+    nhapMang(a, n);
+
+    // Gọi hàm xuất mảng ban đầu
+    printf("\nMang vua nhap la: ");
+    xuatMang(a, n);
+
+    // Gọi hàm sắp xếp
+    sapXepTangDan(a, n);
+
+    // Gọi hàm xuất mảng sau khi sắp xếp
+    printf("Mang sau khi sap xep tang dan: ");
+    xuatMang(a, n);
+
+    return 0;
+}
